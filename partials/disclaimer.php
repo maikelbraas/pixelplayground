91b882efd232947bd71e00b0a06c69877b1d8b38
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 14:36:41 +0200

    disclaimer

--- partials/disclaimer.php
+++ partials/disclaimer.php
@@ -1,8 +1,12 @@
 <div id='disclaimer'>
+    <div>
     <h1>DO NOT FILL IN REAL DATA</h1>
     <b>This site is not secure, it is an example website for first year programming students.</b>
     <b>Encryption hasn't been thought at this stage.</b>
     <b>If you create an account be aware of this. All password are visible.</b>
-    <button id='agree'>I agree</button>
-    <button id='disagree'>I do not agree</button>
+    <div>
+        <button id='agree'>I agree</button>
+        <button id='disagree'>I do not agree</button>
+    </div>
+    </div>
 </div>
