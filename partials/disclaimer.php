c6c275da3b310c72172af9ad335eadf3c2cb59e1
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 15:07:43 +0200

    Update disclaimer.php

--- partials/disclaimer.php
+++ partials/disclaimer.php
@@ -4,7 +4,7 @@
     <b>This site is not secure, it is an example website for first year programming students.</b>
     <b>Encryption hasn't been thought at this stage.</b>
     <b>If you create an account be aware of this. All password are visible.</b>
-    <h2>You're be reminded every hour after accepting!</h2>
+    <h2>You'll be reminded every hour after accepting.</h2>
     <p id="timer">You can agree or disagree in: 5</p>
     </div>
 </div>
