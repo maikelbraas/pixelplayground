f6c120170899fba93af62ec13e910a91003e89de
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 13:37:52 +0200

    show errors

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,5 @@
 
    <footer>
-
    <a href="/"><img src="/images/favicon_io/logo.webp" alt="Logo" width="50" height="50"></a>
       <p>Student example website - 1st year, last project</p>
       <p>Main purpose: CRUD, Game logic</p>
