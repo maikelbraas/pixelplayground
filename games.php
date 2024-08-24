58b62041a0adf6fc16a845bea1f98985157f40be
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 10:20:27 +0200

    different name

--- games.php
+++ games.php
@@ -1,18 +1,18 @@
 <?php require_once 'partials/header.php'; ?>
 <?php  
 $games = [
-    (object)["name" => 'blockchase', 'highscorecapable' => false],
-    (object)["name" => 'connect', 'highscorecapable' => true],
-    (object)["name" => 'flappy', 'highscorecapable' => false],
-    (object)["name" => 'galgje', 'highscorecapable' => true],
-    (object)["name" => 'goalie', 'highscorecapable' => false],
-    (object)["name" => 'shootball', 'highscorecapable' => false],
-    (object)["name" => 'shootThePig', 'highscorecapable' => true],
-    (object)["name" => 'snake', 'highscorecapable' => false],
-    (object)["name" => 'spacelite', 'highscorecapable' => false],
-    (object)["name" => 'tictactoe', 'highscorecapable' => true],
-    (object)["name" => 'wordle', 'highscorecapable' => true],
-    (object)["name" => 'breakout', 'highscorecapable' => false]
+    (object)["name" => 'blockchase', 'displayname' => 'Block chase', 'highscorecapable' => false],
+    (object)["name" => 'connect', 'displayname' => 'Connect 4', 'highscorecapable' => true],
+    (object)["name" => 'flappy', 'displayname' => 'Flappy', 'highscorecapable' => false],
+    (object)["name" => 'galgje', 'displayname' => 'Hangman', 'highscorecapable' => true],
+    (object)["name" => 'goalie', 'displayname' => 'Goalie', 'highscorecapable' => false],
+    (object)["name" => 'shootball', 'displayname' => 'Shoot the balls', 'highscorecapable' => false],
+    (object)["name" => 'shootThePig', 'displayname' => 'Shoot the pigs', 'highscorecapable' => true],
+    (object)["name" => 'snake', 'displayname' => 'Snake', 'highscorecapable' => false],
+    (object)["name" => 'spacelite', 'displayname' => 'Space lite', 'highscorecapable' => false],
+    (object)["name" => 'tictactoe', 'displayname' => 'Tic Tac Toe', 'highscorecapable' => true],
+    (object)["name" => 'wordle', 'displayname' => 'Wordle', 'highscorecapable' => true],
+    (object)["name" => 'breakout', 'displayname' => 'Breakout', 'highscorecapable' => false]
 ];
 ?>
 <main>
@@ -22,7 +22,7 @@ $games = [
         <a href="games/<?= $game->name ?>/index.php">
             <article class="game">
                 <h2><?= $game->name ?></h2>
-                <img src="images/<?= $game->name ?>.png" alt="">
+                <img src="images/<?= $game->displayname ?>.png" alt="">
                 <b>Worden scores opgeslagen: <i><?php if($game->highscorecapable) echo 'Ja'; else echo 'Nee'; ?></i></b>
             </article>
         </a>
