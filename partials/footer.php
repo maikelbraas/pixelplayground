73e130690927af39b7cbcf637be0915d6f82d62d
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 09:57:59 +0200

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,7 @@
 
    <footer>
-      Student example website - 1st year, last project
+      <p>Student example website - 1st year, last project</p>
+      <p>Main purpose: CRUD, Game logic</p>
    </footer> 
 </body>
 </html>
