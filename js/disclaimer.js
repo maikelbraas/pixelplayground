cc28c03f1aa11f2c455877e53af49db7de80a9ba
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 14:52:52 +0200

    Update disclaimer.js

--- js/disclaimer.js
+++ js/disclaimer.js
@@ -5,7 +5,7 @@ if (document.cookie.includes('visitSite=iagree'))
     document.getElementById('disclaimer').style.display = 'none';
 
 agree.addEventListener('click', () => {
-    document.cookie = `visitSite=iagree, secure=true, expires=${new Date.now() + 60 * 60 * 1000}`
+    document.cookie = `visitSite=iagree, secure=true, expires=${Date.now() + 60 * 60 * 1000}`
     document.getElementById('disclaimer').style.display = 'none';
 })
 
