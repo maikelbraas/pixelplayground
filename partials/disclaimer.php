397aa3da6800c8dd03c120013e429a4b1efcaa36
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 17:55:13 +0200

    text change vulnerabilities

--- partials/disclaimer.php
+++ partials/disclaimer.php
@@ -2,8 +2,8 @@
     <div id="inner">
     <h1>DO NOT FILL IN REAL DATA</h1>
     <b>This site is not secure, it is an example website for first year programming students.</b>
-    <b>Encryption hasn't been thought at this stage.</b>
-    <b>If you create an account be aware of this. All password are visible.</b>
+    <b>Encryption and security hasn't been done to showcase vulnerabilities.</b>
+    <b>If you create an account be aware of this. All passwords are visible.</b>
     <h2>You'll be reminded every hour after accepting.</h2>
     <p id="timer">You can agree or disagree in: 5</p>
     </div>
--- partials/footer.php
+++ partials/footer.php
@@ -2,7 +2,7 @@
    <footer>
    <a href="/"><img src="/images/favicon_io/logo.webp" alt="Logo" width="50" height="50"></a>
       <p>Student example website - 1st year, last project</p>
-      <p>Main purpose: CRUD, Game logic</p>
+      <p>Main purpose: Vulnerabilities, CRUD, Game logic</p>
    </footer> 
 <?php require_once 'partials/disclaimer.php'; ?>
 <script src="/js/disclaimer.js" defer></script>
