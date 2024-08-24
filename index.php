8d4aee4ce12a8d535d63b75ead29f00f02ca8bb2
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 09:42:35 +0200

    place

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <h1>Welcome!</h1>
     <section id="index-messages">
         <article class="message">
-            <h2>Welcome!</h2>
             <p>Dit is een website om spelletjes te spelen met vrienden.</p>
             <p>Je kunt ook met je vrienden toernooien maken! Nodig er een paar uit!</p>
             <p>Laat zien wat je kan bij de verschillende games, word jij de uiteindelijke winaar?</p>
