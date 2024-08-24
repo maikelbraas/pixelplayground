8949e7cb9fd3da6e23f4706592482c4cfd17d651
Maikel Braas <maikelbraas@gmail.com>
2024-08-24 13:34:47 +0200

    logos

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,7 @@
 
    <footer>
+
+   <a href="/"><img src="/images/favicon_io/logo.webp" alt="Logo" width="50" height="50"></a>
       <p>Student example website - 1st year, last project</p>
       <p>Main purpose: CRUD, Game logic</p>
    </footer> 
--- partials/header.php
+++ partials/header.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" />
+    <meta name="robots" content="index, nofollow" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="theme-color" content="#121212" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project 4 - Pixel Playground</title>
+    <link
+      rel="icon"
+      type="image/x-icon"
+      href="/images/favicon_io/favicon.ico" />
+    <link
+      rel="apple-touch-icon"
+      href="/images/favicon_io/apple-touch-icon.png" />
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/app.js" defer></script>
 </head>
@@ -13,6 +23,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/partials/functions.php'; ?>
     <header>
         <nav>
+            <img src="/images/favicon_io/logo.webp" alt="Logo" width="50" height="50">
             <p>Welcome <?php if(isset($_SESSION['gebruikersnaam'])) echo $_SESSION['gebruikersnaam'] ?></p>
             <a href="/index.php">Home</a>
             <a href="/games.php">Games</a>
