8c6644a046404ab1e02974625ef1839d0ef9e9a6
Maikel Braas <47853556+mbraas@users.noreply.github.com>
2025-03-12 13:02:16 +0100

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -3,7 +3,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL); ?>
 
 <!DOCTYPE html>
-<html lang="en" data-theme="light">
+<html lang="en" data-theme="dark">
 
 <head>
     <meta charset="UTF-8" />
