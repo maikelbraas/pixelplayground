f72008811b1bd3c60e6611d445a86bb356c03e57
Maikel Braas <47853556+mbraas@users.noreply.github.com>
2025-03-12 11:51:46 +0100

    test

--- partials/header.php
+++ partials/header.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL); ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="robots" content="index, nofollow" />
@@ -12,38 +13,40 @@ error_reporting(E_ALL); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project 4 - Pixel Playground</title>
     <link
-      rel="icon"
-      type="image/x-icon"
-      href="/images/favicon_io/favicon.ico" />
+        rel="icon"
+        type="image/x-icon"
+        href="/images/favicon_io/favicon.ico" />
     <link
-      rel="apple-touch-icon"
-      href="/images/favicon_io/apple-touch-icon.png" />
+        rel="apple-touch-icon"
+        href="/images/favicon_io/apple-touch-icon.png" />
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/app.js" defer></script>
 </head>
-<body>
-    <?php session_start(); 
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/partials/functions.php'; ?>
+<body>
+    <?php session_start();
+    //fsdfdsfsd
+
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/partials/functions.php'; ?>
     <header>
         <nav>
             <img src="/images/favicon_io/logo.webp" alt="Logo" width="50" height="50">
-            <p>Welcome <?php if(isset($_SESSION['gebruikersnaam'])) echo $_SESSION['gebruikersnaam'] ?></p>
+            <p>Welcome <?php if (isset($_SESSION['gebruikersnaam'])) echo $_SESSION['gebruikersnaam'] ?></p>
             <a href="/index.php">Home</a>
             <a href="/games.php">Games</a>
-            <?php if(checkLogin()){ ?>
-            <article>
-                <a href="/profile.php">Profile</a> /
-                <a href="/highscores.php">Highscores</a> /
-                <a href="/friends.php">Friends</a>
-            </article>
+            <?php if (checkLogin()) { ?>
+                <article>
+                    <a href="/profile.php">Profile</a> /
+                    <a href="/highscores.php">Highscores</a> /
+                    <a href="/friends.php">Friends</a>
+                </article>
             <?php } ?>
-            <?php if(!checkLogin()){ ?>
-            <article>
-                <a href="/login.php">Login</a> /
-                <a href="/register.php">Register</a>
-            </article>
-            <?php }else{ ?>
+            <?php if (!checkLogin()) { ?>
+                <article>
+                    <a href="/login.php">Login</a> /
+                    <a href="/register.php">Register</a>
+                </article>
+            <?php } else { ?>
                 <a href="/partials/logout.php">Logout</a>
             <?php } ?>
         </nav>
